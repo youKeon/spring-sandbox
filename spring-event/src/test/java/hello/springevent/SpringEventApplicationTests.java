40f11b4ddf1d48b0269930c0db0dfdc9bd3ef0b9
유건 <k.yu@okestro.com>
2024-10-11 16:24:46 +0900

    chore: Spring 이벤트 리스너 테스트 세팅

---
+++ spring-event/src/test/java/hello/springevent/SpringEventApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
