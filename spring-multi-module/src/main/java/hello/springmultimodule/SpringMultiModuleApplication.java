0657e3e739e90b51833361e14603a3bbf2dbd9ec
유건 <k.yu@okestro.com>
2024-10-11 13:46:10 +0900

    chore: Spring 멀티 모듈 테스트 세팅

---
+++ spring-multi-module/src/main/java/hello/springmultimodule/SpringMultiModuleApplication.java
@@ -0,0 +1,13 @@
+package hello.springmultimodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultiModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMultiModuleApplication.class, args);
+	}
+
+}
---
+++ spring-multi-module/src/test/java/hello/springmultimodule/SpringMultiModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springmultimodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMultiModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
