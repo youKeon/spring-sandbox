e60efbe312623b640795422940d0f7a81e7e88db
유건 <k.yu@okestro.com>
2024-10-14 17:09:19 +0900

    test: Redis 분산락 테스트 코드 추가

--- spring-redis/src/main/java/hello/springredis/lock/Ticket.java
+++ spring-redis/src/main/java/hello/springredis/lock/Ticket.java
@@ -5,10 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,10 +20,16 @@ public class Ticket {
     private Long quantity;
 
     public void increase() {
+        validateCheck();
         this.quantity++;
     }
 
     public void decrease() {
+        validateCheck();
         this.quantity--;
+    }
+
+    private void validateCheck() {
+        if (quantity <= 0) throw new RuntimeException("잔여 수량이 없습니다.");
     }
 }
--- spring-redis/src/test/java/hello/springredis/SpringRedisApplicationTests.java
+++ spring-redis/src/test/java/hello/springredis/SpringRedisApplicationTests.java
@@ -1,13 +1,47 @@
 package hello.springredis;
 
+import hello.springredis.lock.Ticket;
+import hello.springredis.lock.TicketRepository;
+import hello.springredis.lock.TicketService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootTest
 class SpringRedisApplicationTests {
+    @Autowired TicketRepository ticketRepository;
+
+    @Autowired TicketService ticketService;
 
     @Test
-    void contextLoads() {
-    }
+    @DisplayName("test")
+    void test() throws InterruptedException {
+        Long tickId = 1L;
+        ticketRepository.save(new Ticket(tickId, 100L));
 
+        int numberOfThreads = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+        CountDownLatch latch = new CountDownLatch(numberOfThreads);
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            executorService.submit(() -> {
+                try {
+                    ticketService.buyTicket(tickId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Ticket ticket = ticketRepository.findById(tickId).orElseThrow(RuntimeException::new);
+        Assertions.assertThat(ticket.getQuantity()).isZero();
+    }
 }
